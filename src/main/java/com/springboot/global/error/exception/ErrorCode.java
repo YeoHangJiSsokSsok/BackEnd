bcc66965f288816ecf55d91d380798c6e3985509
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2023-09-07 17:22:32 +0900

    ErrorCode 수정

--- src/main/java/com/springboot/global/error/exception/ErrorCode.java
+++ src/main/java/com/springboot/global/error/exception/ErrorCode.java
@@ -17,13 +17,13 @@ public enum ErrorCode {
 
     //Places
     PLACE_NOT_FOUND(400, "P001", "존재 하지 않는 장소 입니다."),
-    REGION_NOT_FOUND(400, "P002", "존재 하지 않는 지역 코드 입니다."),
+    REGION_NOT_FOUND(400, "P002", "존재 하지 않는 지역 입니다."),
     REVIEW_NOT_FOUND(400, "R001", "존재 하지 않는 리뷰 입니다."),
 
     MONTH_NOT_EXIST(400, "M001", "존재 하지 않는 월 입니다."),
 
     //Category
-    CATEGORY_NOT_FOUND(400, "C001", "존재 하지 않는 카테고리입니다."),
+    CATEGORY_NOT_FOUND(400, "C001", "존재 하지 않는 카테고리 입니다."),
     MONTHLY_SUMMARY_NOT_FOUND(400, "S001", "존재 하지 않는 월 별 summary 입니다.")
     ;
 
